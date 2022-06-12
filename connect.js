0f0ccc158652c90ae1b6d27b111bf2a7da076af6
Milda Fauziah <fauziah.milda@gmail.com>
2022-06-12 10:59:20 +0700

    aman

--- connect.js
+++ connect.js
@@ -3,15 +3,15 @@ var mysql = require("mysql");
 
 //membuat koneksi database
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "waste",
-
-  // host: "104.154.30.56",
-  // user: "waste-app-user",
-  // password: "wasteapp101",
+  // host: "localhost",
+  // user: "root",
+  // password: "",
   // database: "waste",
+
+  host: "104.154.30.56",
+  user: "waste-app-user",
+  password: "wasteapp101",
+  database: "waste",
 });
 
 conn.connect((err) => {
